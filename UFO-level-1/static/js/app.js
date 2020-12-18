468a665c388ef21d25af1a04e7f90a3ec4b39dc7
Matthew Kukulski <Matthew.Kukulski@du.edu>
2020-12-18 00:42:14 -0700

    level 1 table appending

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,48 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Loop through each ufo object in the data array
+tableData.forEach((ufoObject) => {
+
+	// Append one table row for each ufo object
+	var row = tbody.append("tr");
+
+	// Use `Object.entries` and forEach to iterate through keys and values of ufo object
+	Object.entries(ufoObject).forEach(([key, value]) => {
+
+		// Append one cell per ufo object value 
+		var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Use a date form in your HTML document and write JavaScript code that will listen
+// for events and search through the date/time column to find rows that match user input.
+
+// Select the button and form
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select(".form-control");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter(ufoObject => ufoObject.datetime === inputValue);
